b84828881bda63c8768d752fa8b954b67294ece8
JesusAlberto19 <l21830082@china.tecnm.mx>
2024-04-05 15:43:54 -0600

    se agrega nuevo texto mishino

---
+++ ProyectoPZA/Catalogo.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style_general.css">
+    <title>Catalogo</title>
+</head>
+<body>
+
+ <header>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+              <h1 class="display-4">Puza</h1>
+              <p class="lead">Catalogo de productos</p>
+            </div>
+          </div>
+ </header>
+
+    <div class="container">
+    <nav aria-label="breadcrumb"> </nav> 
+    </div>
+    
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="menu.php">Menu</a></li>
+        <li class="breadcrumb-item"><a href="menumatprim.php">Materias primas</a></li>
+        <li class="breadcrumb-item"><a href="menuprodterm.php">Productos terminados</a></li>
+        <li class="breadcrumb-item"><a href="menuclientes.php">Clientes</a></li>
+        <li class="breadcrumb-item"><a href="menuprove.php">Proveedores</a></li>
+      </ol> 
+    </nav>
+
+    <main role="main">
+
+  
+
+  <div class="album py-5 bg-light">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+          <img src="img/silla-11.jpeg">
+            <div class="card-body">
+              <p class="card-text">SILLA INFANTIL DE ASIENTO Y RESPALDO DE TRIPLAY DE PINO DE 18MM DE GROSOR ACABADO CON SELLADOR Y LACA. ESTRUCTURA DE TUBO NEGRO DE 1” CALIBRE 18</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                <a href="formula.php" class="btn btn-primary">Cotizar</a>
+                <a href="menuformula.php" class="btn btn-primary">Regresar</a>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+</main>
+
+     
+    
+</body>
+</html>
